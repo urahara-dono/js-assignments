53531392b8a8f093f66a59a075e1401601fc2067
urahara_dono <urahara333@gmail.com>
2016-03-21 15:43:06 +0200

    Fix #3

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -212,16 +212,10 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-    var count = 0;
-    for(var i = 2; i < n; i++) {
-        if(n % i == 0) 
-            count++;
-    }
-    if(count == 0) 
-        return true;
-    else 
-        return false;
+function isPrime(n) {   
+    for (var i = 2; i < n; i++)
+        if(n % i == 0) return false;
+    return true; 
     //throw new Error('Not implemented');
 }
 
